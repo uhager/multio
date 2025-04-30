9273fd7a127aaf5052e2b04e7ce78d09d0656de2
Ulrike Hager <ulrike.hager@ecmwf.int>
2025-04-30 13:21:43 +0000

    remove grib2 yaml based on Stefanie's suggestions.

--- src/multio/action/encode-grib2/EncodeGrib2.cc
+++ src/multio/action/encode-grib2/EncodeGrib2.cc
@@ -112,7 +112,7 @@ MultiOMDict::MultiOMDict(MultiOMDictKind kind) {
 }
 
 void MultiOMDict::toYAML(const std::string& file) {
-    multio_grib2_dict_to_yaml(&dict_, "stdout");
+  //    multio_grib2_dict_to_yaml(&dict_, "stdout");
 }
 
 void MultiOMDict::set(const char* key, const char* val) {
